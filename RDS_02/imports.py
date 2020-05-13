023da4ec65fe5620ecbebaee2e320e22144de155
igel2000 <igel2000@gmail.com>
2020-05-14 00:11:55 +0400

    Доработал проект RDS-02

--- RDS_02/imports.py
+++
@@ -1,9 +0,0 @@
-#импорты
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from itertools import combinations
-from scipy.stats import ttest_ind
-import os
-from collections import Counter
--- RDS_02/tune.py
+++
@@ -1,5 +0,0 @@
-#настроить mathplotlib
-%matplotlib inline
-#настроим pandas
-pd.set_option('display.max_rows', 50) # выведем больше строк
-pd.set_option('display.max_columns', 50) # выведем больше колонок
